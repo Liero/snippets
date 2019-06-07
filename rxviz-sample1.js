9cddbe446d91430607fe7c572aba3803a0479405
Daniel Turan <daniel.turan@hotmail.com>
2019-06-07 13:24:08 +0200

    initial commit

---
+++ rxviz-sample1.js
@@ -0,0 +1,37 @@
+const { concat,timer, Observable, ReplaySubject } = Rx;
+const { take, switchMap } = RxOperators;
+
+const responseDelay = 1000;
+const subscriptionDelay = 500;
+
+function getFromCacheLazy() {
+  return new Observable(s => {
+     s.next('cache');
+     s.complete();
+  })
+}
+function executeRequest() {
+  var request$ = new ReplaySubject(1);
+  setTimeout(() => {
+    request$.next('response');
+    request$.complete();
+  }, responseDelay);
+  return request$;
+}
+
+//1. lazy observable that emit 0 or two value created at time of subscription and then completes
+const cache$ = getFromCacheLazy();
+
+//2.  BehaviorSubject, that emits value in 1-1500ms and completes
+const request$ = executeRequest(); 
+
+//3. comine so that cache is emmited first and request second
+const cacheAndRequest$ = concat(cache$, request$);
+
+
+
+const $subscribeDelayed = timer(subscriptionDelay).pipe(
+  switchMap(_ => cacheAndRequest$))
+
+
+$subscribeDelayed; //last expression must be observable in order to visualize
